88dd73857fa840ff689e0345c8dd229eac90e3d3
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-21 16:16:14 -0400

    commit tree 36083283aa503c7e5118d718a7ff4b051b07e82a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(array, name) {
+    let match = array.filter(element => element.toLowerCase() == name.toLowerCase())
+    return match
+}
+
+function fuzzyMatch(array, name) {
+    let match = array.filter(element => element.indexOf(name) == name.indexOf(name));
+    return match
+}
+
+function matchName(array, str) {
+    let match = array.filter(element => element.name == str)
+    return match
+}
